07b7953a99ee9e7062c8eb30b287d742b8d47df7
RehabAlaa <rehabalaa266199@gmail.com>
2022-12-18 23:48:35 +0200

    initial commit

--- js/index.js
+++ js/index.js
@@ -24,7 +24,12 @@ if (localStorage.getItem("products") != null) {
   displayProducts();
 }
 function addProduct() {
-  if (validationProductsForm() == true) {
+  if (
+    validationProductName() == true &&
+    validationProductPrice() == true &&
+    validationProductCategory() == true &&
+    validationProductDesc() == true
+  ) {
     var products = {
       name: productNameInput.value,
       price: productPriceInput.value,
@@ -36,24 +41,6 @@ function addProduct() {
     localStorage.setItem("products", JSON.stringify(productsContainer));
     clearForm();
     displayProducts();
-  } else {
-    if (productNameInput.value == "") {
-      validName.innerHTML = "**Enter Your Product Name";
-    } else if (productNameRegEx.test(productNameInput.value) == false) {
-      validName.innerHTML = "**invalid Product Name";
-    } else if (productPriceInput.value == "") {
-      validPrice.innerHTML = "**Enter Your Product Price";
-    } else if (productPriceRegEx.test(productPriceInput.value) == false) {
-      validPrice.innerHTML = "**invalid Your Product Price";
-    } else if (productCategoryInput.value == "") {
-      validCategory.innerHTML = "**Enter Your Product Category";
-    } else if (productCategoryRegEx.test(productCategoryInput.value) == false) {
-      validCategory.innerHTML = "**invalid Your Product Category";
-    } else if (productDescInput.value == "") {
-      validDesc.innerHTML = "**Enter Your Product Description";
-    } else if (productDescRegEx.test(productDescInput.value) == false) {
-      validDesc.innerHTML = "**invalid Your Product Description";
-    }
   }
 }
 function clearForm() {
@@ -134,22 +121,92 @@ function searchProducts() {
   document.getElementById("tableBody").innerHTML = container;
 }
 
-function validationProductsForm() {
-  productNameRegEx = /^[A-Z][a-z]{3,8}$/;
-  productPriceRegEx = /^[0-9]+$/;
-  productCategoryRegEx = /^[a-z]{3,8}$/;
-  productDescRegEx = /^[a-z]{3,}$/;
-  if (productNameRegEx.test(productNameInput.value) == true) {
-    if (productPriceRegEx.test(productPriceInput.value) == true) {
-      if (productCategoryRegEx.test(productCategoryInput.value) == true) {
-        if (productDescRegEx.test(productDescInput.value) == true) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    }
-  } else {
+// function validationProductsForm() {
+//   productNameRegEx = /^[A-Z][a-z]{3,8}$/;
+//   productPriceRegEx = /^[0-9]+$/;
+//   productCategoryRegEx = /^[a-z]{3,8}$/;
+//   productDescRegEx = /^[a-z]{3,}$/;
+//   if (productNameRegEx.test(productNameInput.value) == true) {
+//     if (productPriceRegEx.test(productPriceInput.value) == true) {
+//       if (productCategoryRegEx.test(productCategoryInput.value) == true) {
+//         if (productDescRegEx.test(productDescInput.value) == true) {
+//           return true;
+//         } else {
+//           return false;
+//         }
+//       }
+//     }
+//   } else {
+//     return false;
+//   }
+// }
+
+function validationProductName() {
+  productNameRegEx = /^[A-Z][a-z]+$/;
+
+  if (productNameInput.value == "") {
+    validName.innerHTML = "**Enter Your Product Name";
     return false;
+  } else {
+    if (productNameRegEx.test(productNameInput.value) == true) {
+      validName.innerHTML = "";
+      return true;
+    } else {
+      validName.innerHTML =
+        " invalid Product Name [ starts with only one uppercase letter and other letters are lowercase without any spaces]";
+      return false;
+    }
+  }
+}
+
+function validationProductPrice() {
+  productPriceRegEx = /^[0-9]+$/;
+
+  if (productPriceInput.value == "") {
+    validPrice.innerHTML = "**Enter Your Product Price";
+    return false;
+  } else {
+    if (productPriceRegEx.test(productPriceInput.value) == true) {
+      validPrice.innerHTML = "";
+      return true;
+    } else {
+      validPrice.innerHTML = " invalid Product price [ number only ]";
+      return false;
+    }
+  }
+}
+function validationProductCategory() {
+  productCategoryRegEx = /^[a-z]+$/;
+
+  if (productCategoryInput.value == "") {
+    validCategory.innerHTML = "**Enter Your Product Category";
+    return false;
+  } else {
+    if (productCategoryRegEx.test(productCategoryInput.value) == true) {
+      validCategory.innerHTML = "";
+      return true;
+    } else {
+      validCategory.innerHTML =
+        " invalid Product category [ starts with only lowercase letters without any spaces ]";
+      return false;
+    }
+  }
+}
+
+function validationProductDesc() {
+  productDescRegEx = /^[a-z]+$/;
+
+  if (productDescInput.value == "") {
+    validDesc.innerHTML = "**Enter Your Product Description";
+    return false;
+  } else {
+    if (productDescRegEx.test(productDescInput.value) == true) {
+      validDesc.innerHTML = "";
+      return true;
+    } else {
+      validDesc.innerHTML =
+        " invalid Product price [ starts with only lowercase letters without any spaces  ]";
+      return false;
+    }
   }
 }
